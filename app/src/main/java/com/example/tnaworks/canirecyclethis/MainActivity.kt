11a601d6ceee75a011ae017e9660473ec1c61f65
Anton Rubisov <anton.rubisov@c3iot.ai>
2019-03-03 00:40:49 -0800

    reduced to just one activity

--- app/src/main/java/com/example/tnaworks/canirecyclethis/MainActivity.kt
+++ app/src/main/java/com/example/tnaworks/canirecyclethis/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.tnaworks.canirecyclethis
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.media.ExifInterface
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -13,7 +17,10 @@ import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
 import android.os.Build
+import android.view.View
 import com.google.firebase.FirebaseApp
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
 
 
 class MainActivity : AppCompatActivity() {
@@ -37,9 +44,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        anton_test_button.setOnClickListener {
-            dispatchTakePictureIntent()
-        }
+        buttonAccessLocalRecyclingInfo.setOnClickListener(fun(_: View) {
+            val i = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.averagepeopleokaystyle.com"))
+            startActivity(i)
+        })
+
+        dispatchTakePictureIntent()
     }
 
     override fun onDestroy() {
@@ -53,9 +63,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
-            val intent = Intent(this, ResultsActivity::class.java)
-            intent.putExtra("currentPhotoPath", currentPhotoPath)
-            startActivity(intent)
+            val bitmap: Bitmap = getBitmapFromPath(currentPhotoPath)
+
+            setPic(bitmap)
+            identifyImage(bitmap)
         }
     }
 
@@ -116,6 +127,63 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    private fun getBitmapFromPath(path: String): Bitmap {
+        val bitmap = BitmapFactory.decodeFile(path)
+        val exif = ExifInterface(path)
+        val orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED)
+
+//        Log.i("Bitmap", "Orientation is " + orientation.toString())
+
+        val matrix = Matrix()
+        when (orientation) {
+            ExifInterface.ORIENTATION_ROTATE_90 -> matrix.postRotate(90F)
+            ExifInterface.ORIENTATION_ROTATE_180 -> matrix.postRotate(180F)
+            ExifInterface.ORIENTATION_ROTATE_270 -> matrix.postRotate(270F)
+            else -> {
+            }
+        }
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true);
+    }
+
+    private fun setPic(bitmap: Bitmap) {
+        imageViewSubject.setImageBitmap(bitmap)
+    }
+
+    private fun identifyImage(bitmap: Bitmap) {
+        var image: FirebaseVisionImage
+        try {
+            image = FirebaseVisionImage.fromBitmap(bitmap)
+        } catch (e: IOException) {
+            e.printStackTrace()
+            throw e
+        }
+
+        val labeler = FirebaseVision.getInstance().getOnDeviceImageLabeler()
+
+        // Or, to set the minimum confidence required:
+        // val options = FirebaseVisionOnDeviceImageLabelerOptions.Builder()
+        //     .setConfidenceThreshold(0.7f)
+        //     .build()
+        // val labeler = FirebaseVision.getInstance().getOnDeviceImageLabeler(options)
+
+        labeler.processImage(image)
+            .addOnSuccessListener { labels ->
+                // Task completed successfully
+                for (label in labels) {
+                    val text = label.text
+                    // val entityId = label.entityId
+                    val confidence = Math.round(label.confidence * 100.0) / 100.0
+                    textViewLabels.text = "${textViewLabels.text}$text: $confidence\n"
+                }
+            }
+            .addOnFailureListener { e ->
+                // Task failed with an exception
+                // ...
+                e.printStackTrace()
+                throw e
+            }
     }
 
 }
