f6353c766e5fc676aa4d6b55be00fe1b32bc8be3
ted herman <hermanted@gmail.com>
2019-03-02 17:40:20 -0800

    first pass as results screen

--- app/src/main/java/com/example/tnaworks/canirecyclethis/Results.kt
+++ app/src/main/java/com/example/tnaworks/canirecyclethis/Results.kt
@@ -1,12 +1,21 @@
 package com.example.tnaworks.canirecyclethis
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_results.*
 
 class Results : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_results)
+
+        buttonAccessLocalRecyclingInfo.setOnClickListener(fun(_: View) {
+            val i = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.averagepeopleokstyle.com"))
+            startActivity(i)
+        })
     }
 }
