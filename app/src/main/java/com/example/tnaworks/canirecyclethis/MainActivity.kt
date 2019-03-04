eda719601a66c6ebd9b6458cb30b65a5741b0f33
ted herman <hermanted@gmail.com>
2019-03-03 17:00:45 -0800

    Remove YES/NO if no idea wtf it is

--- app/src/main/java/com/example/tnaworks/canirecyclethis/MainActivity.kt
+++ app/src/main/java/com/example/tnaworks/canirecyclethis/MainActivity.kt
@@ -54,6 +54,7 @@ class MainActivity : AppCompatActivity() {
         })
 
         textViewAdvice.setText("")
+        textViewRecyclability.setText("")
 
         dispatchTakePictureIntent()
     }
